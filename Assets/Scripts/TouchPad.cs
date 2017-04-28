34c6bc2a04c386141c15da27d4f605f357eb99f4
Holy-Knight <vnycall74@naver.com>
2017-04-29 04:08:32 +0900

    Change Answer Logic

--- Assets/Scripts/TouchPad.cs
+++ Assets/Scripts/TouchPad.cs
@@ -6,18 +6,56 @@ public class TouchPad : MonoBehaviour {
 
     GameManager gameManager;
 
+    string answer = "";
+
+    //char[] answer = new char[11];
+    int count = 0;
+
 	// Use this for initialization
 	void Start () {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-	}
+
+        //for (int i = 1; i < 10; i++)
+        //    answer[i] = '0';
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-    public void SelectItem(string item)
+    public void SelectItem(int item)
     {
-        gameManager.answer += item;
+        count++;
+
+        if (count == 1)
+            gameManager.answer = "";
+
+        answer += item.ToString();
+
+        if (count == 3)
+        {
+            gameManager.answer = answer;
+
+            answer = "";
+
+            count = 0;
+        }
+
+        //answer[item] = '1';
+
+        //count++;
+
+        //if (count == 3)
+        //{
+        //    gameManager.answer = "";
+
+        //    for (int i = 1; i < 10; i++)
+        //    {
+        //        gameManager.answer += answer[i];
+        //    }
+
+        //    count = 0;
+        //}
     }
 }
