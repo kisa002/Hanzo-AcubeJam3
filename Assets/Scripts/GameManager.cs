d05a347c45d9f307610483780d94a9d7dccb9b5b
Holy-Knight <vnycall74@naver.com>
2017-04-29 00:14:06 +0900

    Add Score
    Add Score

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public int score;
+
+    private Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = score + "점";
+
+        ScoreUp();
+	}
+
+    void ScoreUp()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+            score += 100;
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    /* 쓸려고했는데 그냥 GameManager에서 관리해도 될 것 같기도하고... 아닌가...?
+    private GameManager gameManager;
+    private Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = gameManager.score + "점";
+	}
+    */
+}
