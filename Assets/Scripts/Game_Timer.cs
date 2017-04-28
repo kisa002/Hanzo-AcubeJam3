751add6bea485ab3b46966ba6e93ab42b772e230
Holy-Knight <vnycall74@naver.com>
2017-04-29 00:29:51 +0900

    Add Pause Menu
    [Add]
    Pause
    Resume
    Restart

---
+++ Assets/Scripts/Game_Timer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Game_Timer : MonoBehaviour {
+
+    public Text Timer;
+
+    public int min = 5;
+    public int sec = 0;
+
+    void Start ()
+    {
+        StartCoroutine(Timer_minus());
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Timer.text = (min + " : " + sec);
+    }
+
+    IEnumerator Timer_minus()
+    {
+        yield return new WaitForSeconds(1f);
+        sec--;
+        if(sec <= -1)
+        {
+            sec = 59;
+            min--;
+            if(min <= -1)
+            {
+                min = 0;
+                StopCoroutine(Timer_minus());
+            }
+
+        }
+        StartCoroutine(Timer_minus());
+    }
+}
--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour {
 
+    GameObject pauseMenu;
+
 	// Use this for initialization
 	void Start () {
-		
+        pauseMenu = transform.FindChild("PauseMenu").gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void Stop()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
 }
